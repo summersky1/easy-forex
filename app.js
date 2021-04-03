88e20df6f0178eb91ee9349661e145457367fd36
Ajay <ajay.summer2@gmail.com>
2021-04-03 20:30:19 +0100

    Refactor query params

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const endpoint = 'https://api.ratesapi.io/api/'
-const date = 'latest'
-const currencies = ['GBP','USD','EUR','JPY','CAD','AUD','KRW','SGD','HKD','INR']
-let base = 'GBP'
-let requestUrl = endpoint + date + `?base=${base}` + `&symbols=${currencies.join(',')}`
-
 const displayElement = document.querySelector('#display')
+const endpoint = 'https://api.ratesapi.io/api/'
+const targetCurrencies = ['GBP','USD','EUR','JPY','CAD','AUD','KRW','SGD','HKD','INR']
+
+let baseCurrency = 'GBP'
+let date = 'latest'
+let requestUrl = endpoint + date
+
+const queryParams = {
+    base: baseCurrency,
+    symbols: targetCurrencies.join(',')
+}
 
 const getResults = async () => {  
     try {
-        const response = await axios.get(requestUrl)
+        const response = await axios.get(requestUrl, {
+            params: queryParams
+        })
         renderResults(response.data)
     }
     catch(error) {
