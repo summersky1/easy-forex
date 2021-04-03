949162122f9ce820847cfaddd5324b80d81a1e6b
Ajay <ajay.summer2@gmail.com>
2021-04-03 17:30:25 +0100

    Print raw results from API call to page

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
+const endpoint = 'https://api.ratesapi.io/api/'
+const date = 'latest'
+const currencies = ['GBP','USD','EUR','JPY','CAD','AUD','KRW','SGD','HKD','INR']
+let base = 'GBP'
+let requestUrl = endpoint + date + `?base=${base}` + `&symbols=${currencies.join(',')}`
+
 const displayElement = document.querySelector('#display')
-displayElement.innerText = 'hello world'
+
+const getResults = async () => {  
+    try {
+        const response =  await fetch(requestUrl)
+        if(response.ok){
+            let jsonResponse = await response.json()
+            displayElement.innerHTML = JSON.stringify(jsonResponse)
+        }
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
+
+getResults()
